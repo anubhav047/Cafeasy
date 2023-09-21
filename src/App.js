d2194bf6b618eadc03c400f3c1070716630f2930
barbieagrawal <barbieagrawal1912@gmail.com>
2023-09-21 11:40:40 +0530

    landing page css updated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='m-auto flex flex-col'>
-        <button className='text-4xl mx-2'> Customer </button>
-        <button className='text-4xl mx-2'> Cafe Staff </button>
+    <div className="App flex justify-center items-center h-screen">
+      <div className='m-auto flex flex-col space-y-10'>
+        <button className='text-2xl mx-2 bg-transparent hover:bg-violet-100 text-violet-400 py-3 px-10 rounded-md border-2 border-violet-200'> CUSTOMER </button>
+        <button className='text-2xl mx-2 bg-transparent hover:bg-violet-100 text-violet-400 py-3 px-10 rounded-md border-2 border-violet-200'> CAFE STAFF </button>
       </div>
     </div>
   );
